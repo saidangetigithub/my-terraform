54afda3225a3ce49db4eaf4ac2ef958fc793c97c
saidangeti <saidangeti1443@gmail.com>
2025-02-17 23:35:29 +0530

    learning terraform

--- LEARN-TERRAFORM/resource.tf
+++ LEARN-TERRAFORM/resource.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "frontend" {
   ami           = "ami-0b4f379183e5706b9"
   instance_type = "t3.micro"
-  security_groups = "openall"
+  security_groups = [ "openall" ]
 
   tags = {
     Name = "frontend"
