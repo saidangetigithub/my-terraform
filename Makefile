807a5f199b62e18d49f62914bdb286745d542d7f
saidangeti <saidangeti1443@gmail.com>
2025-02-26 17:29:12 +0530

    added make file

--- Makefile
+++ Makefile
@@ -3,3 +3,6 @@ dev:
 	terraform init -backend-config=dev-env/state.tfvars
 	terraform apply -auto-approve -var-file=dev-env/inputs.tfvars
 
+prod:
+	rm	-rf	terraform	
+
--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "VPC" {
-    source ="/MY-MODULES/VPC"
+    source ="MY-MODULES/VPC"
     vpc_cidr = var.vpc_cidr
     public_subnets = var.public_subnets
     private_subnets = var.private_subnets
