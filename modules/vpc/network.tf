7567df192fbc4bb98335478f6adfe87a859731ec
saidangeti <saidangeti1443@gmail.com>
2025-02-26 19:18:10 +0530

    added make file

--- modules/vpc/network.tf
+++ modules/vpc/network.tf
@@ -43,7 +43,7 @@ resource "aws_eip" "eip" {
 
 resource "aws_nat_gateway" "ngw" {
   allocation_id = aws_eip.eip.id
-  subnet_id     = var.public_subnets[0].id
+  subnet_id     = aws_subnet.public_subnets[0].id
   tags = {
     Name = "ngw-${var.env}"
   }
