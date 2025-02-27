0e190ae266f27f4e188e0236add45b9a07468474
saidangeti <saidangeti1443@gmail.com>
2025-02-27 16:48:04 +0530

    added loaad balancer:

--- modules/vpc/output.tf
+++ modules/vpc/output.tf
@@ -4,11 +4,11 @@ output "expense_id" {
 }
 
 output "public_subnets" {
-    value = aws_subnet.public_subnets.*.id
+    value = aws_subnet.public.*.id
   
 }
 
 output "private_subnets" {
-    value = aws_subnet.private_subnets.*.id
+    value = aws_subnet.private.*.id
   
 }
