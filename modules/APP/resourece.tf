472e4d0210c95cf6a3e7e2fe7e8c4a1d75f3324a
saidangeti <saidangeti1443@gmail.com>
2025-02-28 19:22:06 +0530

    aadded new changes

--- modules/APP/resourece.tf
+++ modules/APP/resourece.tf
@@ -17,3 +17,17 @@ resource "aws_launch_template" "foo" {
 
  # user_data = filebase64("${path.module}/example.sh")
 }
+
+
+
+resource "aws_autoscaling_group" "bar" {
+  availability_zones = ["us-east-1a"]
+  desired_capacity   = 1
+  max_size           = 1
+  min_size           = 1
+
+  launch_template {
+    id      = aws_launch_template.foo.id
+    version = "$Latest"
+  }
+}
